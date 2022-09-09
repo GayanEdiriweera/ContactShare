2b7a45f22c904d4769ba4512222856a4764fff36
GayanEdiriweera <gayan.ediriweera@gmail.com>
2022-09-09 13:39:09 +0930

    Fix for modal dismiss on ios

--- js/main.js
+++ js/main.js
@@ -21,16 +21,19 @@ header.addEventListener("click", () => {
   }
 });
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modalBackground) {
-    if (passMode) {
-      modalBackground.style.display = "none";
-      header.style.height = "4rem";
-      passMode = false;
+let eventsArray = ["click", "touchstart", "touchend"];
+eventsArray.forEach(function (eventName) {
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener(eventName, function (event) {
+    if (event.target === modalBackground) {
+      if (passMode) {
+        modalBackground.style.display = "none";
+        header.style.height = "4rem";
+        passMode = false;
+      }
     }
-  }
-};
+  });
+});
 
 let userData = {
   firstname: "",
