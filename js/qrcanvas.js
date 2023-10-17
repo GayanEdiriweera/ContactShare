f56e6c2dffcfcb77f5d8ebd2916eee3d0c24269b
GayanEdiriweera <gayan.ediriweera@gmail.com>
2023-10-17 20:02:39 +1030

    removing border from qr code display

--- js/qrcanvas.js
+++ js/qrcanvas.js
@@ -11,22 +11,12 @@ class qrcanvas {
 function drawCodeToCanvas(canvas, code, colors) {
   const { dark, light } = colors;
   const context = canvas.getContext("2d");
-  canvas.width = code.size + 4;
-  canvas.height = code.size + 4;
+  canvas.width = code.size;
+  canvas.height = code.size;
   for (let y = 0; y < canvas.height; y++) {
     for (let x = 0; x < canvas.width; x++) {
-      context.fillStyle = getFillStyle(x, y, canvas, code, dark, light);
+      context.fillStyle = code.modules[y][x] ? dark : light;
       context.fillRect(x, y, 1, 1);
     }
   }
-}
-
-function getFillStyle(x, y, canvas, code, dark, light) {
-  const isOuterOutline =
-    x == 0 || y == 0 || x == canvas.width - 1 || y == canvas.height - 1;
-  const isInnerOutline =
-    x == 1 || y == 1 || x == canvas.width - 2 || y == canvas.height - 2;
-  if (isOuterOutline) return dark;
-  if (isInnerOutline) return light;
-  return code.modules[y - 2][x - 2] ? dark : light;
 }
